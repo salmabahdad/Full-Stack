ba82806c4d30c374d9409ec49ca13102ffcbc7bd
Code Wizard <mac@XXs-MacBook-Air.local>
2025-06-24 13:51:52 +0100

    Ajout du projet avec backend

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+app.use('/api/stuff', stuffRoutes);
+
+// in routes/stuff.js
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.post('/', stuffCtrl.createThing);
+
+mongoose.connect('mongodb+srv://salmabh12:salma.bahdad2003@cluster0.bvxmdjz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+module.exports = app;
